91a5475d011ca2f2cd63c60ce1b887e1c1a4eb4f
Jad K. Haddad <jadkhaddad@gmail.com>
2024-11-26 13:43:24 +0100

    feat(examples): esp32 async latest version init
    Signed-off-by: Jad K. Haddad <jadkhaddad@gmail.com>

---
+++ examples/esp32/async - new/src/bin/async_main.rs
@@ -0,0 +1,34 @@
+#![no_std]
+#![no_main]
+
+use embassy_executor::Spawner;
+use embassy_time::{Duration, Timer};
+use esp_backtrace as _;
+use esp_hal::prelude::*;
+use log::info;
+
+#[main]
+async fn main(spawner: Spawner) {
+    let peripherals = esp_hal::init({
+        let mut config = esp_hal::Config::default();
+        config.cpu_clock = CpuClock::max();
+        config
+    });
+
+    esp_println::logger::init_logger_from_env();
+
+    let timer0 = esp_hal::timer::timg::TimerGroup::new(peripherals.TIMG1);
+    esp_hal_embassy::init(timer0.timer0);
+
+    info!("Embassy initialized!");
+
+    // TODO: Spawn some tasks
+    let _ = spawner;
+
+    loop {
+        info!("Hello world!");
+        Timer::after(Duration::from_secs(1)).await;
+    }
+
+    // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/v0.22.0/examples/src/bin
+}
