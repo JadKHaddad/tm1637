d9d98e086394854549a39a3cc0ad5e98ebd232ae
Jad K. Haddad <jadkhaddad@gmail.com>
2024-11-29 22:00:35 +0100

    fix(docs): type in identity.rs
    Signed-off-by: Jad K. Haddad <jadkhaddad@gmail.com>

--- tm1637/src/identity.rs
+++ tm1637/src/identity.rs
@@ -1,6 +1,6 @@
 /// Identity trait.
 ///
-/// Used to trick the compiler while using [`duplicate_item`] to implement `async` and `blocking` versions of the same module.
+/// Used to trick the compiler while using [`duplicate_item`](::duplicate::duplicate_item) to implement `async` and `blocking` versions of the same module.
 /// Using this trait, we can write normal rust code that can also be formatted by `rustfmt`.
 pub trait Identity: Sized {
     fn identity(self) -> Self {
