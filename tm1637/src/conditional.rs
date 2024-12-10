6413e52240d432487f02eda0880abaa857df8fe5
Jad K. Haddad <jadkhaddad@gmail.com>
2024-12-10 18:12:39 +0100

    fix(clippy): conditional.rs
    Signed-off-by: Jad K. Haddad <jadkhaddad@gmail.com>

--- tm1637/src/conditional.rs
+++ tm1637/src/conditional.rs
@@ -1,9 +1,7 @@
-use ::embedded_hal::digital::InputPin;
-
 /// Conditional input pin trait.
 ///
-/// Used for conditional requirement of the [`InputPin`] trait based on the `ack` feature.
-/// - Every [`InputPin`] implements this trait, if the `ack` feature is enabled.
+/// Used for conditional requirement of the [`InputPin`](::embedded_hal::digital::InputPin) trait based on the `ack` feature.
+/// - Every [`InputPin`](::embedded_hal::digital::InputPin) implements this trait, if the `ack` feature is enabled.
 /// - Everything implements this trait, if the `ack` feature is not enabled.
 pub trait ConditionalInputPin<ERR> {
     /// Is the input pin low?
@@ -13,14 +11,18 @@ pub trait ConditionalInputPin<ERR> {
 }
 
 #[cfg(feature = "ack")]
-impl<ERR, T> ConditionalInputPin<ERR> for T
-where
-    T: InputPin<Error = ERR>,
-{
-    fn is_low(&mut self) -> Result<bool, ERR> {
-        self.is_low()
+const _: () = {
+    use ::embedded_hal::digital::InputPin;
+
+    impl<ERR, T> ConditionalInputPin<ERR> for T
+    where
+        T: InputPin<Error = ERR>,
+    {
+        fn is_low(&mut self) -> Result<bool, ERR> {
+            self.is_low()
+        }
     }
-}
+};
 
 #[cfg(not(feature = "ack"))]
 impl<ERR, T> ConditionalInputPin<ERR> for T {}
