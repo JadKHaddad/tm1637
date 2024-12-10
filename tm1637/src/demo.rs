bfcddf1a044c3bf9f72da5279a1ea9e321fac382
Jad K. Haddad <jadkhaddad@gmail.com>
2024-12-10 18:36:37 +0100

    refactor(demo): deleted demo.rs
    Signed-off-by: Jad K. Haddad <jadkhaddad@gmail.com>

--- tm1637/src/demo.rs
+++
@@ -1,7 +0,0 @@
-//! This module contains a demo implementation for the `TM1637` device.
-//!
-//! This module is only available when the `demo` feature of this
-//! library is activated.
-
-pub mod asynch;
-pub mod blocking;
