6cd7197370762c2108f1d1f286f9fa43e3cd1376
Jad K. Haddad <jadkhaddad@gmail.com>
2025-05-19 19:55:02 +0200

    docs(lib): added main example to lib.rs
    Signed-off-by: Jad K. Haddad <jadkhaddad@gmail.com>

--- tm1637/src/lib.rs
+++ tm1637/src/lib.rs
@@ -1,8 +1,33 @@
 //! A platform agnostic driver to interface with the `TM1637` (7-segment display) using the [`embedded-hal`](embedded_hal) and [`embedded-hal-async`](embedded_hal_async) traits.
 //!
+//! ```rust
+//! use tm1637_embedded_hal::{mock::Noop, Brightness, TM1637Builder};
+//!
+//! let mut tm = TM1637Builder::new(Noop, Noop, Noop)
+//!     .brightness(Brightness::L3)
+//!     .delay_us(100)
+//!     // Blocking or async mode
+//!     .build_blocking::<6>();
+//!
+//! // Clear the display and set brightness
+//! tm.init().ok();
+//!
+//! // High-Level fluent API
+//! tm.options()
+//!     .str("HELLO. ruSt.")
+//!     .scroll()
+//!     .linear()
+//!     .finish()
+//!     .run();
+//!
+//! // Or Low-Level API
+//! let bytes = &[0b00000110, 0b01011011, 0b01001111, 0b01100110]; // `1234`
+//!
+//! tm.display_slice(0, bytes).ok();
+//! ```
+//!
 //! # Features
 //!
-//! The following features are available:
 //! - `ack`: enables the driver to use the `InputPin` trait for the `DIO` pin and wait for the acknowledgment signal from the display.
 //! - `defmt`: implements `defmt::Format` for structs and enums.
 
