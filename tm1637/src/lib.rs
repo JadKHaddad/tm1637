e37c617db9c149cdb586757151d3c0bc57ceddfa
Jad K. Haddad <jadkhaddad@gmail.com>
2025-05-19 19:33:48 +0200

    feat(visibility): made ConditionalInputPin pub(crate)
    Signed-off-by: Jad K. Haddad <jadkhaddad@gmail.com>

--- tm1637/src/lib.rs
+++ tm1637/src/lib.rs
@@ -31,7 +31,7 @@ pub mod tokens;
 
 pub use brightness::Brightness;
 pub use builder::TM1637Builder;
-pub use conditional::ConditionalInputPin;
+pub(crate) use conditional::ConditionalInputPin;
 pub use device::TM1637;
 pub use error::Error;
 pub(crate) use identity::Identity;
